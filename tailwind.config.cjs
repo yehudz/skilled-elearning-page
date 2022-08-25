d3b4e1a78ef2e4e484b79d7a268359ccfe4e4326
yehudz <yehuda.zeytim@gmail.com>
2022-08-24 18:34:49 -0600

    [SELLP-1] Create Tailwind theme
    AC1 Add color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'purple': '#666CA3',
+      'black': "#13183F",
+      'grey': '#83869A',
+      'pink': "#F74780",
+      'light-pink': 'FFA7C3',
+      'white': "#FFFFFF",
+      'pink1': "#F02AA6",
+      'pink2': "#FF6F48",
+      'purple1': '#4851FF',
+      'purple2': '#F02AA6'
+    },
     extend: {},
   },
   plugins: [],
