97870f6301b0d1a933cbef9d669d8db2386320b9
yehudz <yehuda.zeytim@gmail.com>
2022-08-24 20:03:22 -0600

    [SELLP-2] Create hero section
    Add initial testing

---
+++ cypress/e2e/hero-section.cy.js
@@ -0,0 +1,29 @@
+describe('Hero Section', ()=> {
+  beforeEach(()=> {
+    cy.visit('/')
+    cy.viewport(350, 540)
+  })
+  it('should have a hero container', ()=> {
+    cy.get('[data-cy="hero-container"]')
+    .should('exist')
+  })
+  it('should have big header', ()=> {
+    cy.get('[data-cy="hero-header"]')
+    .invoke('text')
+    .should('not.be.empty')
+  })
+  it('should have description', ()=> {
+    cy.get('[data-cy="hero-description"]')
+    .invoke('text')
+    .should('not.be.empty')
+  })
+  it('should have get started button', ()=> {
+    cy.get('[data-cy="get-started-button"]')
+    .should('not.be.empty')
+  })
+  it('should have hero image', ()=> {
+    cy.get('[data-cy="hero-container"]')
+    .find('img')
+    .should('be.visible')
+  })
+})
